a00e885e8ee30dcbfa1af092f9df5aafd593fba0
Speeed912 <shreerangbhavsar912@gmail.com>
2026-03-13 12:07:13 +0530

    Add placeholder Request and Send Notes screens
    added stubs or empty classes to request and send notes temporarily to test chat screen which i will be making next

---
+++ lib/screens/notes/request_notes_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RequestNotesScreen extends StatelessWidget {
+  const RequestNotesScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Requst Notes')),
+      body: const Center(child: Text('Coming Soon')),
+    );
+  }
+}
---
+++ lib/screens/notes/send_notes_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SendNotesScreen extends StatelessWidget {
+  const SendNotesScreen({super.key});
+  @override
+  Widget build(BuildContext content) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Send Notes')),
+      body: const Center(child: Text('Coming soon')),
+    );
+  }
+}
